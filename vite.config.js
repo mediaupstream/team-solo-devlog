9f4dba79afc68e8c5a425e388a67503b612062ee
Derek Anderson <derek@mediaupstream.com>
2025-03-27 23:07:23 -0500

    fix: thing

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteReact(), tailwindcss()],
-  base: '/team-solo-devlog/',
   test: {
     globals: true,
     environment: 'jsdom',
